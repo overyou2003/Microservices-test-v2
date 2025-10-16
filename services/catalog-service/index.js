913e1ed8cb0493a186a912556776a8de3456b876
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2025-10-17 01:55:27 +0700

    dorm

--- services/catalog-service/index.js
+++ services/catalog-service/index.js
@@ -42,7 +42,7 @@ const PRODUCTS = [
     img: 'https://images.unsplash.com/photo-1511746315387-c4a76990fdce?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
   { id: 'p4', name: 'Adidas Superstar', price: 2920, stock: 50,
     img: 'https://images.unsplash.com/photo-1593287073863-c992914cb3e3?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-  { id: 'p5', name: 'Puma Running Shoes', price: 1999, stock: 20,
+  { id: 'p5', name: 'Puma Running Shoes', price: 2099, stock: 20,
     img: 'https://images.unsplash.com/photo-1715692965422-280fe6566146?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
   { id: 'p6', name: 'Puma Sportswear', price: 1200, stock: 15,
     img: 'https://images.unsplash.com/photo-1629753897877-522619845842?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
