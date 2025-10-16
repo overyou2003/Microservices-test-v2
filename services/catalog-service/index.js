7ea21cd2ee0b629d21508ddf73c6ff2401dfa4d9
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2025-10-17 01:55:17 +0700

    dorm

--- services/catalog-service/index.js
+++ services/catalog-service/index.js
@@ -48,7 +48,7 @@ const PRODUCTS = [
     img: 'https://images.unsplash.com/photo-1629753897877-522619845842?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
   { id: 'p7', name: 'Reebok Classic', price: 22220, stock: 25,
     img: 'https://images.unsplash.com/photo-1726317219474-7f8a98b53743?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-  { id: 'p8', name: 'Reebok Fitness Gear', price: 1500, stock: 40,
+  { id: 'p8', name: 'Reebok Fitness Gear', price: 150, stock: 40,
     img: 'https://images.unsplash.com/photo-1599989571484-100fa5168bf3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
 ];
 
