a8567d2bd3e02c5204caa21d4c203288ecc81f7d
overyou2003 <s6506021620032@email.kmutnb.ac.th>
2025-10-19 23:30:40 +0700

    dorm

--- services/catalog-service/index.js
+++ services/catalog-service/index.js
@@ -44,7 +44,7 @@ const PRODUCTS = [
     img: 'https://images.unsplash.com/photo-1593287073863-c992914cb3e3?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
   { id: 'p5', name: 'Puma Running Shoes', price: 2099, stock: 20,
     img: 'https://images.unsplash.com/photo-1715692965422-280fe6566146?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-  { id: 'p6', name: 'Puma Sportswear', price: 12100, stock: 15,
+  { id: 'p6', name: 'Puma Sportswear', price: 1210, stock: 15,
     img: 'https://images.unsplash.com/photo-1629753897877-522619845842?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
   { id: 'p7', name: 'Reebok Classic', price: 2220, stock: 25,
     img: 'https://images.unsplash.com/photo-1726317219474-7f8a98b53743?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
